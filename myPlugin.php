a23400400dc9dbc2ef7b68e0b3a4717875ad517b
kelvinconnery <kel_vin71@yahoo.com>
2013-10-18 04:20:46 +0800

    adding comment

--- myPlugin.php
+++ myPlugin.php
@@ -2,20 +2,20 @@
     /* 
     Plugin Name: Web Statistic 
     Description: Plugin for displaying popular post during certain period
-    Author: 
+    Author: Kelvin Connery
     Version: 1.0 
     Author URI:  
     */
-	
+	//authentication for user and gather the google analytic info
 	function pop_admin(){
-		getPopularPost('hwhwhwhwhw123@gmail.com','123admin123','77879940');
+		getPopularPost('hwhwhwhwhw123@gmail.com','123admin123','77879940'); //pass the parameter to getPopularPost
 	}
-	
+	//function for get the popular post
 	function getPopularPost($ga_email,$ga_pass,$ga_profile_id){
-		require 'gapi.class.php';
-		
+		require 'gapi.class.php'; //include the gapi.class.php
+		//create new gapi object
 		$ga = new gapi($ga_email,$ga_pass);
-		
+		//displaying data according to the filter
 		fetchData($ga, $ga_profile_id, '-7 day','tab-one');
 		fetchData($ga, $ga_profile_id, '-1 month','tab-two');
 		fetchData($ga, $ga_profile_id, '-1 year','tab-three');
@@ -23,32 +23,32 @@
 		
 		
 	}
-	
+	//function for displaying
 	function fetchData($ga,$ga_profile_id,$range,$css_id){
 				
-
-		$endTime = new DateTime('+8 hour');
+		
+		$endTime = new DateTime('+8 hour');//+8 according to malaysian time zone
 		$endTimeStr = date_format($endTime,'Y-m-d');
 		
-		$startTime = $endTime->modify($range);
+		$startTime = $endTime->modify($range);//set the lower bound
 		$startTimeStr = date_format($startTime, 'Y-m-d');
 		
 		$ga->requestReportData($ga_profile_id,array('pagePath'),array('pageviews', 'uniquePageviews'),array('-pageviews'),'pagePath != /',$startTimeStr,$endTimeStr,1,10);
 		
 		echo "<div class='wrap' id=$css_id>From: ".$startTimeStr.' until '.$endTimeStr.'<br/>'; 
-		
+		//displaying result
 		echo '<table>';
 		foreach($ga->getResults() as $result)
 		{
 			
-			$resultStr = explode("/",$result);
-			$num = (count($resultStr)-2);
+			$resultStr = explode("/",$result);//hide unnecessary data
+			$num = (count($resultStr)-2);//get the index of page name
 			echo '<tr><td>'.$resultStr[$num].'</td><td>';
 			echo $result->getPageviews().'</td></tr>';
 		}
 		echo '</table>';
 	}
-			
+	//adding plugin to wp admin		
 	function popular_post_actions() {
 		add_options_page("Popular Post", "Popular Post","manage_options" ,__FILE__, "pop_admin");
 	}
